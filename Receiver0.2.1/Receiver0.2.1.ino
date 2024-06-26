a87b00ea1acca38ffb3e7b2d71bbbb322a0916fb
Thisuka Kodithuwakku <nimneththisuka@gmail.com>
2024-06-27 01:02:45 +0530

    Update Receiver0.2.1.ino

--- Receiver0.2.1/Receiver0.2.1.ino
+++ Receiver0.2.1/Receiver0.2.1.ino
@@ -8,7 +8,9 @@
 #define motorpin 3
 #define buzzer 7
 #define pushbutton 8
+RF24 radio(CE_PIN, CSN_PIN);
 
+bool motor_pin_state=false;
 bool level0_status = false;
 bool level6_status = false;
 int val[6][8] = { { 0, 0, 0, 1, 0, 0, 0, 0 },
@@ -19,6 +21,12 @@ int val[6][8] = { { 0, 0, 0, 1, 0, 0, 0, 0 },
                   { 0, 0, 0, 0, 0, 0, 0, 0 } };
 int level;
 
+uint8_t address[][6] = { "1Node", "2Node" };
+bool radioNumber = false;  // 0 uses address[0] to transmit, 1 uses address[1] to transmit
+bool role = false;         // true = TX node, false = RX node
+short frame[3];
+int str_time;
+int end_time;
 void led(int j) {
   for (int i = 0; i < 8; i++) {
     digitalWrite(ds, val[j][i]);
@@ -36,13 +44,66 @@ void buzzr(int time){
 }
 
 
-RF24 radio(CE_PIN, CSN_PIN);
-uint8_t address[][6] = { "1Node", "2Node" };
-bool radioNumber = false;  // 0 uses address[0] to transmit, 1 uses address[1] to transmit
-bool role = false;         // true = TX node, false = RX node
-short frame[3];
-int str_time;
-int end_time;
+void indicator(int level) {
+  if (level == 11 && !level0_status) {
+    led(5);
+    level0_status = true;
+    motor_pin_state=true;
+    digitalWrite(motorpin, HIGH);  //level 6
+    digitalWrite(buzzer, HIGH);    //level 5
+    delay(600);                    //
+    digitalWrite(buzzer, LOW);     //
+    delay(1000);                   //
+  } else if (level == 22) {         //level 4
+    led(5);                        //
+    led(0);                        //level 3
+    
+  } else if (level == 33) {         //
+    led(5);                        //level 33
+    led(1);                        //
+  } else if (level == 44) {         //level 22
+    led(5);                        //level 11
+    led(2);
+    level0_status = false;
+  } else if (level == 55) {
+    led(5);
+    led(3);
+    level6_status = false;
+  } else if (level ==66 ) {
+    led(5);
+    led(4);
+    
+  } else if (level == 77 && !level6_status) {
+    led(4);
+    level6_status = true;
+    motor_pin_state=false;
+    digitalWrite(motorpin, LOW);
+    delay(100);
+    digitalWrite(buzzer, HIGH);
+    delay(300);
+    digitalWrite(buzzer, LOW);
+    delay(1000);
+  }
+}
+void nosignal(){
+  buzzr(100);
+  buzzr(300);
+  led(4);
+  delay(100);
+  led(5);
+}
+
+void airflow(){
+  buzzr(100);
+  led(4);
+  delay(100);
+  led(5); 
+}
+
+
+
+
+
 
 void setup() {
   for (int i = 0; i < 4; i++) {
@@ -75,11 +136,27 @@ void setup() {
 
 void loop() {
   end_time=millis();
-  if(end_time-str_time>100000000000){
-    while(!radio.available()){
-      nosignal();
+  if(motor_pin_state){
+    if(end_time-str_time>60000){
+      digitalWrite(motorpin,LOW);
+      while(!radio.available()){
+        nosignal();
+      }
+    }
+  }else{
+    if(end_time-str_time>100000000000){
+      while(!radio.available()){
+        nosignal();
+        bool switch1=digitalRead(8);
+        if(switch1){
+          break;
+        }
+      }
     }
   }
+
+
+
   if (radio.available()) {
     radio.read(&frame, sizeof(frame));
     str_time=end_time;
@@ -92,64 +169,4 @@ void loop() {
     delay(1000);
     digitalWrite(buzzer, LOW);
   }
-}
-void indicator(int level) {
-  if (level == 11 && !level0_status) {
-    led(5);
-    level0_status = true;
-    digitalWrite(motorpin, HIGH);  //level 6
-    digitalWrite(buzzer, HIGH);    //level 5
-    delay(600);                    //
-    digitalWrite(buzzer, LOW);     //
-    delay(1000);                   //
-  } else if (level == 22) {         //level 4
-    led(5);                        //
-    led(0);                        //level 3
-    
-  } else if (level == 33) {         //
-    led(5);                        //level 33
-    led(1);                        //
-  } else if (level == 44) {         //level 22
-    led(5);                        //level 11
-    led(2);
-    level0_status = false;
-  } else if (level == 55) {
-    led(5);
-    led(3);
-    level6_status = false;
-  } else if (level ==66 ) {
-    led(5);
-    led(4);
-    
-  } else if (level == 77 && !level6_status) {
-    led(4);
-    level6_status = true;
-    digitalWrite(motorpin, LOW);
-    delay(100);
-    digitalWrite(buzzer, HIGH);
-    delay(300);
-    digitalWrite(buzzer, LOW);
-    delay(1000);
-  }
-}
-
-
-
-void nosignal(){
-  buzzr(100);
-  buzzr(300);
-  led(4);
-  delay(100);
-  led(5);
-
-
-}
-
-
-
-void airflow(){
-  buzzr(100);
-  led(4);
-  delay(100);
-  led(5); 
 }
